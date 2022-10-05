1520b278d40df013683f7b1b9d390a7ea67f5aa9
Jani Kapyla <kapyla.jani@gmail.com>
2022-10-05 11:21:13 +0300

    Produce all data from _joined script

--- processDemo/README.txt
+++ processDemo/README.txt
@@ -1,11 +1,30 @@
-./run.bat name_of_the_take
+TODO: Switch to ps
+
+-------------------------------------------------
+
+./run_one_frame.bat name_of_the_take
 
 Processes quickly 1 frame in the demo session
 
-Produces following files into take folder:
+Produces:
 *_mesh.mvx
 meshes\ .obj
 plys\ .ply
 pngs\ color.png
 pngs\ depth.png
 pngs\ calibration_parameters.txts
+
+--------------------------------------------------
+
+./run.bat name_of_the_take (-pngs 1/0 -plys 1/0 -ply 1/0 -mesh 1/0 -start 0-n -end -1-n)
+
+Processes quickly 1 frame in the demo session
+
+Optional arguments - 1 process - 0 don't process:
+-pngs: Images per camera in pngs\*_color.png, pngs\*_depth.png pngs\calibration_parameters.txts
+-plys: Plys from each camera in plys\*.ply
+-ply: One big ply per frame in ply\*.ply
+-mesh: Does poisson and produces objs in meshes\*_.obj
+-start: Process from index ... (default=0=first frame)
+-end: ... to index (default=-1=last frame)
+
--- processDemo/run.bat
+++ processDemo/run.bat
@@ -1,13 +1,62 @@
-@echo off
-SETLOCAL EnableDelayedExpansion 
+Rem TODO: Switch to ps
 
+@echo off
+
+Rem Update to latest build
 cd C:\ring_it\builds\MVAgent.05-09-2022_60ce56b.Windows.Release
 
 set directory=C:\storage
 set graph_directory=C:\Users\Civit\Desktop\process_demo\Graphs
-
-set take_name=%1
 set take_directory=%directory%\%take_name%
+
+Rem Parse arguments --------- TODO: Argument error
+set take_name=%1
+set pngs=1
+set plys=1
+set ply=1
+set mesh=1
+set start=0
+set end=-1
+SHIFT
+
+:loop
+IF NOT "%1"=="" (
+    IF "%1"=="-pngs" (
+        SET pngs=%2
+        SHIFT
+    )
+    IF "%1"=="-plys" (
+        SET plys=%2
+        SHIFT
+    )
+	IF "%1"=="-ply" (
+        SET ply=%2
+        SHIFT
+    )
+	IF "%1"=="-mesh" (
+        SET mesh=%2
+        SHIFT
+    )
+	IF "%1"=="-start" (
+        SET start=%2
+        SHIFT
+    )
+	IF "%1"=="-end" (
+        SET end=%2
+        SHIFT
+    )
+	SHIFT
+    GOTO :loop
+)
+echo %take_name%
+echo %pngs%
+echo %plys%
+echo %ply%
+echo %mesh%
+echo %start%
+echo %end%
+goto End
+Rem -------------------------
 
 if EXIST %take_directory%\%take_name%_joined.mvx (
 	echo Process: %take_directory%\%take_name%_joined.mvx
@@ -16,34 +65,45 @@ if EXIST %take_directory%\%take_name%_joined.mvx (
 	GOTO End
 )
 
+if %pngs%=="1" (
+	Rem Produce RGBs for cam 16 in this take (2 pngs per camera)
+	.\XmlGraphRunner.exe "%graph_directory%\03_exportPNGs.xml" ^
+		--input "%directory%\!take_name!\!take_name!_joined.mvx" ^
+		--output "%directory%\!take_name!\pngs" ^
+		--start %start% ^
+		--end %end%
+)
 
-Rem Run poisson for first frame
-.\XmlGraphRunner.exe "%graph_directory%\01_joined_to_mesh_poission_runner.xml" ^
-	--start 0 ^
-	--end 1 ^
-	--input "%take_directory%\!take_name!_joined.mvx" ^
-	--output "%take_directory%\!take_name!_mesh.mvx"		
+if %plys%=="1" (
+	Rem Produce plys
+	.\XmlGraphRunner.exe "%graph_directory%\04_exportPLYs.xml" ^
+		--input "%directory%\!take_name!\!take_name!_joined.mvx" ^
+		--output "%directory%\!take_name!\plys\!take_name!" ^
+		--start %start% ^
+		--end %end%
+)
 
-Rem export .obj
-.\XmlGraphRunner.exe "%graph_directory%\02_readTOobj.xml" ^
-	--input "%take_directory%\!take_name!_mesh.mvx" ^
-	--output "%take_directory%\meshes\!take_name!"
+if %ply%=="1" (
+	Rem Produce 1 ply containing everything
+	.\XmlGraphRunner.exe "%graph_directory%\05_exportPLY.xml" ^
+		--input "%directory%\!take_name!\!take_name!_joined.mvx" ^
+		--output "%directory%\!take_name!\plys\!take_name!" ^
+		--start %start% ^
+		--end %end%		
+)
 
-Rem Produce RGBs for cam 16 in this take (2 pngs per camera)
-.\XmlGraphRunner.exe "%graph_directory%\03_exportPNGs.xml" ^
-	--input "%directory%\!take_name!\!take_name!_joined.mvx" ^
-	--output "%directory%\!take_name!\pngs" ^
-	--end 1
+if %mesh%=="1" (
+	Rem Run poisson for first frame
+	.\XmlGraphRunner.exe "%graph_directory%\01_joined_to_mesh_poission_runner.xml" ^
+		--input "%take_directory%\!take_name!_joined.mvx" ^
+		--output "%take_directory%\!take_name!_mesh.mvx" ^
+		--start %start% ^
+		--end %end%
 
-Rem Produce plys
-.\XmlGraphRunner.exe "%graph_directory%\04_exportPLYs.xml" ^
-	--input "%directory%\!take_name!\!take_name!_joined.mvx" ^
-	--output "%directory%\!take_name!\plys\!take_name!" ^
-	--end 1
-	
-Rem Produce 1 ply containing everything
-.\XmlGraphRunner.exe "%graph_directory%\05_exportPLY.xml" ^
-	--input "%directory%\!take_name!\!take_name!_joined.mvx" ^
-	--output "%directory%\!take_name!\plys\!take_name!" ^
-	--end 1		
+	Rem export .obj
+	.\XmlGraphRunner.exe "%graph_directory%\02_readTOobj.xml" ^
+		--input "%take_directory%\!take_name!_mesh.mvx" ^
+		--output "%take_directory%\meshes\!take_name!"
+)
+
 :End
